b49bd7ec4a75c71cc8acc0b2f1c515734d50b318
Anirudh <anirudh@semiotic.ai>
2025-02-14 15:21:08 -0800

    refactor: fix order of combine and verify_aggregate functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
-use ark_bls12_381::{Bls12_381, Fr, G1Affine, G1Projective, G2Affine, g1::Config};
+use ark_bls12_381::{g1::Config, Bls12_381, Fr, G1Affine, G1Projective, G2Affine};
 use ark_ec::pairing::Pairing;
 use ark_ec::{
-    AffineRepr,
     hashing::{
-        HashToCurve, HashToCurveError, curve_maps::wb::WBMap,
-        map_to_curve_hasher::MapToCurveBasedHasher,
+        curve_maps::wb::WBMap, map_to_curve_hasher::MapToCurveBasedHasher, HashToCurve,
+        HashToCurveError,
     },
+    AffineRepr,
 };
 use ark_ff::fields::field_hashers::DefaultFieldHasher;
-use ark_std::{UniformRand, Zero, ops::Mul};
+use ark_std::{ops::Mul, UniformRand, Zero};
 use sha2::Sha256;
 
 pub struct PK {
@@ -26,11 +26,12 @@ fn hash_to_curve(msg: &[u8], domain: &[u8]) -> Result<G1Affine, HashToCurveError
     Ok(hash_point)
 }
 
-pub fn setup() -> (PK, Fr) {
-}
+pub fn setup() -> (PK, Fr) {}
 
 pub fn sign(sk: Fr, pk: &PK, id: &[u8], index: usize, m: Fr) -> Result<G1Affine, HashToCurveError> {
 }
+
+pub fn combine(weights: &Vec<Fr>, signatures: Vec<G1Affine>) -> G1Affine {}
 
 pub fn verify(
     pk: &PK,
@@ -48,9 +49,6 @@ pub fn verify_aggregate(
     m: Fr,
     signature: G1Affine,
 ) -> Result<bool, HashToCurveError> {
-}
-
-pub fn combine(weights: &Vec<Fr>, signatures: Vec<G1Affine>) -> G1Affine {
 }
 
 #[cfg(test)]
@@ -84,7 +82,9 @@ mod tests {
         let aggregate_m = m_vec.iter().zip(&weights).map(|(m, w)| m.mul(w)).sum();
 
         // Verify the aggregate signature
-        assert!(verify_aggregate(&pk, id, weights.clone(), aggregate_m, aggregate_signature).unwrap());
+        assert!(
+            verify_aggregate(&pk, id, weights.clone(), aggregate_m, aggregate_signature).unwrap()
+        );
 
         // Test with wrong message
         let wrong_m = Fr::rand(&mut ark_std::test_rng());
